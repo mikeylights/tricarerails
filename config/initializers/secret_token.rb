e786f94884dddcc3c423445d68531d09fd2c7f7d
tricare <tricarehealth@gmail.com>
2013-07-26 15:49:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tricarerails::Application.config.secret_token = '489451da5fc5a3fe110c8195af9b7f61a9c7c6bcee7ffd7f7369731d2e076925cceeb2643df054d2b9501c8fda17e68fbf4c4011e6445081cdc884166e5614ff'
